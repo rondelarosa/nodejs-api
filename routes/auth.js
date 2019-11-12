154900fa3d92b05cfc5f76ef332ec9f88fbe30d3
Ronnie dela-Rosa <ronniedelarosa@hotmail.com>
2019-11-12 07:57:50 -0500

    register user

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,23 @@ import express from 'express';
 import User from '../models/user';
 const router = express.Router();
 
-router.post('/register', (req, res)=> {
-	const user = new User({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-	});
-});
+const registerUser = () => {
+	return (req, res) => {
+		const user = new User({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+		});
+		try {
+			const savedUser = user.save();
+			savedUser.then( s => res.send(s));
+		}
+		catch (error) {
+			res.status(400).send(error);
+		}
+	};
+};
+
+router.post('/register', registerUser()); 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 // Import Routes
 import authRoute from './routes/auth';
+
+dotenv.config();
 
 const app = express();
 
@@ -14,6 +14,9 @@ const app = express();
 mongoose.connect(process.env.DB_CONNECT,
 	{ useNewUrlParser: true, useUnifiedTopology: true },
 	() => console.log('connected to db!'));
+
+//Middleware
+app.use(express.json());
 
 // Route Middlewares
 app.use('/api/user', authRoute);
