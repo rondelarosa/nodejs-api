2a222db89f8f88d5c4da4053e7ce72f654850904
Ron <ronniedelarosa@hotmail.com>
2019-11-12 10:41:17 -0500

    validate in db if email exist

--- routes/auth.js
+++ routes/auth.js
@@ -5,21 +5,25 @@ import userValidation from '../validations/user';
 const router = express.Router();
 
 const registerUser = () => {
-	return (req, res) => {
+	return async (req, res) => {
 		// validate
 		const { error } = userValidation(req.body);
-		if(error) return res.status(400).send(error.details[0].message);
+		if (error) return res.status(400).send(error.details[0].message);
 
+		// check if user in database
+		const findUser = await User.findOne({ email: req.body.email });
+		if (findUser) return res.status(400).send('Email alredy exist');
+
+		// crate new user
 		const user = new User({
 			name: req.body.name,
 			email: req.body.email,
-			password: req.body.password,
+			password: req.body.password
 		});
 		try {
-			const savedUser = user.save();
-			savedUser.then( s => res.send(s));
-		}
-		catch (error) {
+			const savedUser = await user.save();
+			savedUser.then(s => res.send(s));
+		} catch (error) {
 			res.status(400).send(error);
 		}
 	};
